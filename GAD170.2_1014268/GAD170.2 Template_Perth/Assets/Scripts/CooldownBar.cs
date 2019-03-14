15562b340725e92b93ff7dc712320a060cd33ab9
Sammy <1014268@student.sae.edu.au>
2019-03-14 16:46:18 +0800

    Update_14.03.19
    Speed bar now depletes over time

---
+++ GAD170.2_1014268/GAD170.2 Template_Perth/Assets/Scripts/CooldownBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CooldownBar : MonoBehaviour
+{
+    public float cooldown = 10;
+    public float cooldownTimer;
+    public GameObject Speed;
+
+    void Start()
+    {
+        cooldownTimer = cooldown;
+        
+    }
+
+    void Update()
+    {
+        if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
+
+        if (cooldownTimer < 0)
+        {
+            cooldownTimer = 0;
+            Attack();
+            cooldownTimer = cooldown;
+        }
+        Scale();
+    }
+
+       void Scale()
+    {
+        transform.localScale = new Vector3(cooldownTimer/cooldown, 1, 1);
+    }
+       
+    
+    void Attack()
+    {
+        Debug.Log("In theory... they just attacked...");
+    }
+}
